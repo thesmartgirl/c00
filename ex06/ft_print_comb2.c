bd0d4bfad03ed017e10dfe6d94448c76233b6825
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-05-03 21:51:37 +0300

    Create ft_print_comb2.c

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print2(int i)
+{
+  if (i < 10) {
+    ft_putchar('0');
+    ft_putchar(i + '0');
+  }
+  else
+  {
+    ft_putchar((i / 10) +'0');
+    ft_putchar((i % 10) + '0');
+  }
+}
+
+void	ft_print_comb2(void)
+{
+	int i;
+	int j;
+
+    i = 0;
+    while (i <= 98) {
+        j = i + 1;
+        while(j <= 99)
+        {
+            ft_print2(i);
+            ft_putchar(' ');
+            ft_print2(j);
+            ft_putchar(',');
+            ft_putchar(' ');
+            j++;
+        }
+    i++;
+  }
+}
