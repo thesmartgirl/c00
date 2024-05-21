7711ba8b37faf3ac99a47e0cb6a519f2c00eea18
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-05-21 22:10:45 +0300

    Update ft_putnbr.c

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -4,26 +4,22 @@ void    ft_putchar(char c)
 	write (1, &c, 1);
 }
 
-void    write_digits(int nb)
-{
-  if(nb > 10)
-    write_digits(nb / 10);
-  ft_putchar((nb%10)+'0');
-}
-
 void    ft_putnbr(int nb)
 {
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return;
+	}
     if(nb < 0)
     {
-      if (nb == -2147483648)
-  			write(1, "-2147483648", 11);
-    	else
-    	{
-            ft_putchar('-');
-            nb = nb * -1;
-            write_digits(nb);
-      }
-    }
-    else
-        write_digits(nb);
-}
+    		ft_putchar('-');
+				nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		nb = nb % 10;
+	}
+	ft_putchar(nb + 48);
+	}
