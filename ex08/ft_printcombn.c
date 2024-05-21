e8845e6c709272ac5c4f8dffe23f1a4531a2164f
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-05-21 22:55:02 +0300

    Update ft_printcombn.c

--- ex08/ft_printcombn.c
+++ ex08/ft_printcombn.c
@@ -1,56 +1,53 @@
-#include <unistd.h>
-
+#include<unistd.h>
 void	ft_putchar(char c)
 {
 	write (1, &c, 1);
 }
-void    write_digits(int nb)
+
+void	print(int *comb, int n)
 {
-  if(nb > 10)
-    write_digits(nb / 10);
-  ft_putchar((nb%10)+'0');
+	int	i;
+
+    i = 0;
+	while (++i < n)
+		if (comb[i - 1] >= comb[i])
+			return;
+	i = 0;
+	while (i < n)
+	{
+		ft_putchar(comb[i] + 48);
+		i++;
+	}
+	if (comb[0] < (10 - n))
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
 }
 
-void    ft_putnbr(int nb)
+void	ft_print_combn(int n)
 {
-    if(nb < 0)
-    {
-      if (nb == -2147483648)
-  			write(1, "-2147483648", 11);
-    	else
-    	{
-            ft_putchar('-');
-            nb = nb * -1;
-            write_digits(nb);
-      }
-    }
-    else
-        write_digits(nb);
-}
+	int	i;
+	int	comb[10];
 
-void find_first(int n)
-{
-  if (n > 1) {
-    find_first(n - 1);
-  }
-  ft_putchar(n + '0');
-}
-
-void find_last(int n)
-{
-  ft_putchar(n + '0');
-  if (n > 1) {
-    find_last(n - 1);
-  }
-}
-
-void ft_print_combn(int n) {
-  int i;
-
-  i = find_first(n);
-  while (i <= find_last(n))
-  {
-    ft_putnbr(i);
-    i++;
-  }
+	if (n > 9 || n < 1)
+		return ;
+	i = -1;
+	while (++i < n)
+		comb[i] = i;
+	while (comb[0] <= (10 - n))
+	{
+		print(comb, n);
+		comb[n - 1]++;
+		i = n - 1;
+		while (i)
+		{
+			if (comb[i] > 9)
+			{
+				comb[i - 1]++;
+				comb[i] = 0;
+			}
+			i--;
+		}
+	}
 }
